a762f0aa1959cd4d6113ca0b0f11c445c407aadc
astudic <astudic@kth.se>
2021-01-30 14:21:59 +0100

    Issue #1
    This adds the agreed file structure as well as Main and Decide classes.
    A review of input and variable placement is still needed.

---
+++ src/Decide.java
@@ -0,0 +1,52 @@
+public class Decide {
+
+    Decide() {
+        boolean[] CMV = new boolean[15];
+    }
+
+    // licCond functions modify this.CMV to avoid passing parameters
+    private void licCond0() {
+    }
+    private void licCond1() {
+    }
+    private void licCond2() {
+    }
+    private void licCond3() {
+    }
+    private void licCond4() {
+    }
+    private void licCond5() {
+    }
+    private void licCond6() {
+    }
+    private void licCond7() {
+    }
+    private void licCond8() {
+    }
+    private void licCond9() {
+    }
+    private void licCond10() {
+    }
+    private void licCond11() {
+    }
+    private void licCond12() {
+    }
+    private void licCond13() {
+    }
+    private void licCond14() {
+    }
+
+    private static boolean[][] computePUM() {
+        return new boolean[15][15];
+    }
+
+    private static boolean[] computeFUV() {
+        return new boolean[15];
+    }
+
+    // calls all functions and returns the answer
+    public boolean runDecide() {
+        return false;
+    }
+
+}
