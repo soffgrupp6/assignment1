4d7765b69a00433799c7b380a43f356014f4b2e8
Timmy Lindholm <timmylindholm@MBPsomthorTimmy.lan>
2021-02-01 22:42:57 +0100

    LIC2 condition implemented, with unit tests

--- src/decider/Conditions.java
+++ src/decider/Conditions.java
@@ -21,6 +21,38 @@ public class Conditions {
         return false;
     }
     public boolean licCond2() {
+        int[][] c = {{0,0}, {0,0}, {0,0}}; // Three consecutive points
+        double disC0C1, disC1C2, disC0C2, cosC1, angle;
+
+        if(points.length < 3)
+            throw new IllegalArgumentException("Invalid number of points");
+
+        for(int i = 0; i <= points.length - 3; i++) {
+             /* Add points */
+            c[0] = points[i];
+            c[1] = points[i + 1];
+            c[2] = points[i + 2];
+
+            /* Make sure points does not coincide */
+            if(c[0][0] == c[1][0] && c[0][1] == c[1][1])
+                continue;
+            if(c[2][0] == c[1][0] && c[2][1] == c[1][1])
+                continue;
+
+            /* Calculate the distances between the three points */
+            disC0C1 = Math.sqrt(Math.pow(c[0][0] - c[1][0], 2) + Math.pow(c[0][1] - c[1][1], 2));
+            disC1C2 = Math.sqrt(Math.pow(c[1][0] - c[2][0], 2) + Math.pow(c[1][1] - c[2][1], 2));
+            disC0C2 = Math.sqrt(Math.pow(c[0][0] - c[2][0], 2) + Math.pow(c[0][1] - c[2][1], 2));
+
+            /* Here we use the cosine formula to calculate the cosine of the vertex of c1 */
+            cosC1 = (Math.pow(disC0C2, 2) - Math.pow(disC1C2, 2) - Math.pow(disC0C1, 2)) / (-1 * disC1C2 * disC0C1);
+
+            /* Now we take the arccos to get the angle */
+            angle = Math.acos(cosC1);
+
+            if(angle < (Math.PI - params.EPSILON) || angle > (Math.PI + params.EPSILON))
+                return true;
+        }
         return false;
     }
     public boolean licCond3() {
--- test/TestConditions.java
+++ test/TestConditions.java
@@ -19,4 +19,55 @@ public class TestConditions {
     public void testCond0isFalse() {
         assertFalse("Condition 0 is not false", cond.licCond0());
     }
+
+    // Test that condition 2 is false when coinciding
+    @Test
+    public void testCond2isFalse() {
+        int[][] points = { {12, 13}, {12, 13}, {0, 1}, {-3, 3} };
+        Parameters params = new Parameters(1,2,3,4,2,10,11,15,3,2,2,2,2,2,2,2,2,2,2);
+        cond = new Conditions(points, params);
+
+        assertFalse("Condition 2 is not false", cond.licCond2());
+    }
+
+    // Test that condition 2 is true when angle = pi/2 and epsilon = 1
+    @Test
+    public void testCond2isTrue() {
+        int[][] points = { {0, 1}, {0, 0}, {1, 0} };
+        Parameters params = new Parameters(1,2,3,4,1,10,11,15,3,2,2,2,2,2,2,2,2,2,2);
+        cond = new Conditions(points, params);
+
+        assertTrue("Condition 2 is not true", cond.licCond2());
+    }
+
+    // Test that condition 2 is true when angle = pi/2 and epsilon = 1 but they are not the first points in the list
+    @Test
+    public void testCond2isTrueButNotFirst() {
+        int[][] points = {{0, 0}, {0, 0}, {0, 1}, {0, 0}, {1, 0} };
+        Parameters params = new Parameters(1,2,3,4,1,10,11,15,3,2,2,2,2,2,2,2,2,2,2);
+        cond = new Conditions(points, params);
+
+        assertTrue("Condition 2 is not true", cond.licCond2());
+    }
+
+    // Test that condition 2 is false when angle = pi/2 and epsilon = 2
+    @Test
+    public void testCond2isFalseEpsilon2() {
+        int[][] points = { {0, 1}, {0, 0}, {1, 0} };
+        Parameters params = new Parameters(1,2,3,4,2,10,11,15,3,2,2,2,2,2,2,2,2,2,2);
+        cond = new Conditions(points, params);
+
+        assertFalse("Condition 2 is not true", cond.licCond2());
+    }
+
+    // Test that condition 2 gives exception when number of points is < 3
+    @Test(expected = IllegalArgumentException.class)
+    public void testCond2YieldsExceptionWhenFewPoints() {
+        int[][] points = { {0, 1}, {0, 0}};
+        Parameters params = new Parameters(1,2,3,4,2,10,11,15,3,2,2,2,2,2,2,2,2,2,2);
+        cond = new Conditions(points, params);
+
+        cond.licCond2();
+    }
+
 }
