3c8bd5a2a72b2e1b8d1552c8c7bed8d904ab715e
Christian Stjernberg <christian@stjernberg.com>
2021-02-01 11:47:57 +0100

    Refactor LIC condition to separate class
    Refactor condition calculations to have a public API for testing.
    This change also moves source files to decider package so classes can access each other over the files.
    
    Resolves issue #20

---
+++ src/decider/Conditions.java
@@ -0,0 +1,62 @@
+package decider;
+
+/**
+ * Conditions calculate if the given points fulfill
+ * the specified conditions based on given parameters
+ */
+public class Conditions {
+
+    int[][] points;
+    Parameters params;
+
+    Conditions(int[][] points, Parameters parameters) {
+        this.points = points;
+        this.params = parameters;
+    }
+
+    public boolean licCond0() {
+        return false;
+    }
+    public boolean licCond1() {
+        return false;
+    }
+    public boolean licCond2() {
+        return false;
+    }
+    public boolean licCond3() {
+        return false;
+    }
+    public boolean licCond4() {
+        return false;
+    }
+    public boolean licCond5() {
+        return false;
+    }
+    public boolean licCond6() {
+        return false;
+    }
+    public boolean licCond7() {
+        return false;
+    }
+    public boolean licCond8() {
+        return false;
+    }
+    public boolean licCond9() {
+        return false;
+    }
+    public boolean licCond10() {
+        return false;
+    }
+    public boolean licCond11() {
+        return false;
+    }
+    public boolean licCond12() {
+        return false;
+    }
+    public boolean licCond13() {
+        return false;
+    }
+    public boolean licCond14() {
+        return false;
+    }
+}
--- src/decider/Main.java
+++ src/decider/Main.java
@@ -1,3 +1,5 @@
+package decider;
+
 public class Main {
     public static void main(String[] args) {
         Decider decision = new Decider();
