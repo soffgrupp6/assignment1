eccc70dd895f97b1f0e26d134fe99c047a094593
Daniel <danne_1998@hotmail.se>
2021-02-03 00:23:00 +0100

    implement condition 12 and add test cases

--- src/decider/Conditions.java
+++ src/decider/Conditions.java
@@ -266,7 +266,25 @@ public class Conditions {
         return false;
     }
     public boolean licCond12() {
-        return false;
+        if(points.length < 3)
+            return false;
+
+        int[] a,b;
+        float xDis, yDis;
+        boolean cond1 = false, cond2 = false;
+
+        for(int i = 0; i < points.length - params.K_PTS - 1; i++){
+            a = points[i];
+            b = points[params.K_PTS + 1 + i];
+            xDis = b[0] - a[0];
+            yDis = b[1] - a[1];
+            if(Math.sqrt(Math.pow(xDis, 2) + Math.pow(yDis, 2)) > params.LENGTH1)
+                cond1 = true;
+            if(Math.sqrt(Math.pow(xDis, 2) + Math.pow(yDis, 2)) < params.LENGTH2)
+                cond2 = true;
+        }
+        
+        return cond1 && cond2;
     }
     public boolean licCond13() {
         return false;
