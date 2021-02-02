c07f634509e5485de1aca96402414291a8939873
Christian <cjgst@kth.se>
2021-02-02 13:55:46 +0100

    Implement condition 6 and add test cases

--- src/decider/Conditions.java
+++ src/decider/Conditions.java
@@ -54,6 +54,52 @@ public class Conditions {
         return false;
     }
     public boolean licCond6() {
+
+        if(points.length < 3)
+           return false;
+
+        int[] first;
+        int[] last;
+
+        double xDis, yDis;
+
+        // Do this for all cases of K_PTS consecutive points
+        for (int i = 0; i <= points.length - params.K_PTS; i++) {
+            first = points[i];
+            last = points[i + params.K_PTS - 1];
+
+            // If the first and last point coincide
+            if (first[0] == last[0] && first[1] == last[1]) {
+                boolean hasDistanceSmallerThanDist = false;
+                for (int j = i+1; j < (i + params.K_PTS - 1); j++) {
+                    // Calculate distance between points
+                    xDis = points[j][0] - first[0];
+                    yDis = points[j][1] - first[1];
+                    if (Math.sqrt(Math.pow(xDis, 2) + Math.pow(yDis, 2)) <= params.DIST) {
+                        hasDistanceSmallerThanDist = true;
+                    }
+                }
+                if (!hasDistanceSmallerThanDist)
+                    return true;
+
+            } else {
+                double numerator;
+                double denominator;
+                for (int j = i+1; j < (i + params.K_PTS - 1); j++) {
+                    // Calculate distance to line defined by the first and last points
+                    // reference: https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
+                    numerator = Math.abs((last[0] - first[0]) * (first[1] - points[j][1])
+                        - (first[0] - points[j][0]) * (last[1] - first[1]));
+                    xDis = last[0] - first[0];
+                    yDis = last[1] - first[1];
+                    denominator = Math.sqrt(Math.pow(xDis, 2) + Math.pow(yDis, 2));
+
+                    if ((numerator / denominator) > params.DIST) {
+                        return true;
+                    }
+                }
+            }
+        }
         return false;
     }
     public boolean licCond7() {
--- test/TestConditions.java
+++ test/TestConditions.java
@@ -45,4 +45,81 @@ public class TestConditions {
         cond = new Conditions(points, params);
         cond.licCond0();
     }
+
+    // Test that condition 6 is true for a simple case
+    @Test
+    public void testCond6simpleTrue() {
+        int[][] points = { {0, 0}, {30, 30}, {0, 15}, {-3, 3} };
+        Parameters params = new Parameters(20,2,3,4,1,10,11,10,3,2,3,3,3,2,2,2,2,2,2);
+
+        cond = new Conditions(points, params);
+
+        assertTrue("Condition 6 is not true for simple case", cond.licCond6());
+    }
+
+    // Test that condition 6 is false for a simple case
+    @Test
+    public void testCond6simpleFalse() {
+        int[][] points = { {0, 0}, {1, 10}, {0, 15}, {-3, 3} };
+        Parameters params = new Parameters(20,2,3,4,1,10,11,10,3,2,3,3,3,2,2,2,2,2,2);
+
+        cond = new Conditions(points, params);
+
+        assertFalse("Condition 6 is not false for simple case", cond.licCond6());
+    }
+
+    // Test that condition 6 is false when distance is exactly DIST
+    @Test
+    public void testCond6FalseForExactlyDistanceDIST() {
+        int[][] points = { {0, 0}, {10, 10}, {0, 15} };
+        Parameters params = new Parameters(20,2,3,4,10,10,10,10,3,2,3,3,3,2,2,2,2,2,2);
+
+        cond = new Conditions(points, params);
+
+        assertFalse("Condition 6 is not false for distance = DIST", cond.licCond6());
+    }
+
+    // Test that condition 6 is false for coinciding points
+    @Test
+    public void testCond6FalseForCoincidingPoints() {
+        int[][] points = { {10, 10}, {0, 0}, {9, 8}, {-3, -2}, {0, 0} };
+        Parameters params = new Parameters(20,2,3,4,4,4,4,4,3,2,4,4,4,2,2,2,2,2,2);
+
+        cond = new Conditions(points, params);
+
+        assertFalse("Condition 6 is not false for coinciding points", cond.licCond6());
+    }
+
+    // Test that condition 6 is true for coinciding points
+    @Test
+    public void testCond6TrueForCoincidingPoints() {
+        int[][] points = { {10, 10}, {0, 0}, {9, 8}, {-3, -4}, {0, 0} };
+        Parameters params = new Parameters(20,2,3,4,4,4,4,4,3,2,4,4,4,2,2,2,2,2,2);
+
+        cond = new Conditions(points, params);
+
+        assertTrue("Condition 6 is not true for coinciding points", cond.licCond6());
+    }
+
+    // Test that condition 6 is true for complex case
+    @Test
+    public void testCond6TrueForComplexCase() {
+        int[][] points = { {10, 10}, {0, 0}, {9, 8}, {-3, -4}, {0, 0}, {1, 1}, {7, 9}, {-15, -9} };
+        Parameters params = new Parameters(20,2,3,4,10,10,10,10,3,2,4,5,4,2,2,2,2,2,2);
+
+        cond = new Conditions(points, params);
+
+        assertTrue("Condition 6 is not true for many points", cond.licCond6());
+    }
+
+    // Test that condition 6 is false for complex case
+    @Test
+    public void testCond6FalseForComplexCase() {
+        int[][] points = { {10, 10}, {0, 0}, {9, 8}, {-3, -4}, {0, 0}, {1, 1}, {2, 3}, {-15, -9} };
+        Parameters params = new Parameters(20,2,3,4,50,50,50,50,3,2,4,5,4,2,2,2,2,2,2);
+
+        cond = new Conditions(points, params);
+
+        assertFalse("Condition 6 is not false for many points", cond.licCond6());
+    }
 }
