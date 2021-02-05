72272832e848c0ea8d6eeb3894fd924a968794cc
Christian <cjgst@kth.se>
2021-02-05 11:42:40 +0100

    Add tests for Decider function
    Adds the large feature tests for the entire program.
    Resolves #50

--- test.sh
+++ test.sh
@@ -1,6 +1,5 @@
 # /bin/bash
 
-javac -cp .:junit-4.13.1.jar:hamcrest-core-1.3.jar:src test/TestConditions.java
+javac -cp .:junit-4.13.1.jar:hamcrest-core-1.3.jar:src test/*.java
 
-java -cp .:junit-4.13.1.jar:hamcrest-core-1.3.jar:src:test org.junit.runner.JUnitCore TestConditions
-
+java -cp .:junit-4.13.1.jar:hamcrest-core-1.3.jar:src:test org.junit.runner.JUnitCore TestConditions TestDecider
--- test/TestConditions.java
+++ test/TestConditions.java
@@ -7,13 +7,6 @@ public class TestConditions {
 
     Conditions cond;
 
-    @Before
-    public void setUp() {
-        int[][] points = { {12, 13}, {31, 213}, {0, 1}, {-3, 3} };
-        Parameters params = new Parameters(1,2,3,4,1,10,11,15,3,2,2,2,2,2,2,2,2,2,2);
-        cond = new Conditions(points, params);
-    }
-
     // Test that condition 0 is false
     @Test
     public void testCond0isFalse() {
